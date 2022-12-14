7ee22b430991e0031b0ca98a0b067dfcf98939a5
stas <nestas67@mail.ru>
2022-12-14 18:44:20 +0300

    task

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,31 @@
+import java.util.Locale;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        task1();
+        task3();
+    }
+
+    public static void task1() {
+        System.out.println("Задача 1");
+        String firstName = "Ivan";
+        String middleName = "Ivanovich";
+        String lastName = "Ivanov";
+        String fullName = lastName + " " + firstName + " " + middleName;
+        System.out.printf("ФИО сотрудника — %s\n", fullName);
+        System.out.println();
+        task2(fullName);
+    }
+
+    public static void task2(String fullName) {
+        System.out.println("Задача 2");
+        System.out.println("Данные ФИО сотрудника для заполнения отчета — " + fullName.toUpperCase());
+        System.out.println();
+    }
+
+    public static void task3() {
+        System.out.println("Задача 3");
+        String fullName = "Иванов Семён Семёнович";
+        System.out.println("Данные ФИО сотрудника — " + fullName.replace('ё', 'е'));
     }
 }
